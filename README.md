7211e1b17e3630af1dd7ed95941e5cc8dd0a2cd9
Joseph <Jbonventre95@gmail.com>
2022-11-13 20:16:33 -0500

    Flatiron Logo added

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My Personal Website
 
-website link: https://bonavoot.github.io/my-nothing-personnel-website/
+website link: https://bonavoot.github.io/my-virtual-business-card/
 
 Hello! Welcome to my personal site! This is a small demonstration of the HTML, CSS and JS I
 am capable of tying together to form a functioning website
--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 // Light and Dark mode selectors
 const lightAndDarkMode = document.getElementById("lightAndDarkMode");
-const header = document.getElementById("header-text")
-const sunAndMoonImg = document.getElementById("")
+const header = document.getElementById("header-text");
+const sunAndMoonImg = document.getElementById("");
 const names = document.getElementById("name");
 
 // Changes CSS color properties of selectors above
 lightAndDarkMode.addEventListener("click", () => {
+  if (lightAndDarkMode.className === "sun") {
+    //changes font and background colors
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    header.style.color = "white";
+    names.style.borderColor = "black";
+    // Change light/dark mode image
+    lightAndDarkMode.src = "./images/moon.png";
+    lightAndDarkMode.className = "moon";
+  } else if (lightAndDarkMode.className === "moon") {
+    document.body.style.backgroundColor = "rgb(30, 30, 30)";
+    document.body.style.color = "white";
+    header.style.color = "black";
+    names.style.borderColor = "white";
+    lightAndDarkMode.src = "./images/sun.png";
+    lightAndDarkMode.className = "sun";
+  }
+});
 
-    if(lightAndDarkMode.className === "sun"){
-        document.body.style.backgroundColor = "white";
-        document.body.style.color = "black";
+const sketchBoard = document.getElementById("sketch-board");
 
-        header.style.color = "white"
-        
-        names.style.borderColor = "black"
-
-        lightAndDarkMode.src = "./images/moon.png"
-        lightAndDarkMode.className = "moon"
-
-    } else if(lightAndDarkMode.className === "moon") {
-        document.body.style.backgroundColor = "rgb(30, 30, 30)";
-        document.body.style.color = "white";
-       
-        header.style.color = "black"
-       
-        names.style.borderColor = "white"
-        
-        lightAndDarkMode.src = "./images/sun.png"
-        lightAndDarkMode.className = "sun"
-    }
-})
-
+function createSketchBoard() {
+  for (let i = 50; i < 1; i--) {}
+}
